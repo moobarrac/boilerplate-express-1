ee39bdbd1ef758702dd91a5eff805793f2b8b925
moobarrac <wahabajibola01@gmail.com>
2024-01-30 15:39:55 +0000

    fix styling and public path

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 
 const publicPath = __dirname + "/public";
 const filePath = __dirname + "/views/index.html";
-app.use(express.static(publicPath));
+app.use("/public", express.static(publicPath));
 app.get("/", function (req, res) {
   res.sendFile(filePath);
 });
