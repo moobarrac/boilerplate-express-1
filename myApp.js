7c69db5b998b3a11860c3474cc653dcbb612f10b
moobarrac <wahabajibola01@gmail.com>
2024-01-30 13:26:34 +0000

    add static middleware to myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require("express");
 let app = express();
 
+const publicPath = __dirname + "public";
 const filePath = __dirname + "/views/index.html";
+app.use(express.static(publicPath));
 app.get("/", function (req, res) {
   res.sendFile(filePath);
 });
