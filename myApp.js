5c2586c6f1e5762cb58016ed0552ac2a039d2372
moobarrac <wahabajibola01@gmail.com>
2024-01-30 15:35:42 +0000

    fix styling in html link

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let app = express();
 
-const publicPath = __dirname + "public";
+const publicPath = __dirname + "/public";
 const filePath = __dirname + "/views/index.html";
 app.use(express.static(publicPath));
 app.get("/", function (req, res) {
