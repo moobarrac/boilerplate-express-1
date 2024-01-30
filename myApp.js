3e1a89d0488d038fb6d8407552407bcfe5638d1a
moobarrac <wahabajibola01@gmail.com>
2024-01-30 13:04:33 +0000

    update myApp.js to send file

--- myApp.js
+++ myApp.js
@@ -1,10 +1,9 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello World");
-
+const filePath = __dirname + "/views/index.html";
 app.get("/", function (req, res) {
-  res.send("Hello Express");
+  res.sendFile(filePath);
 });
 
 module.exports = app;
