68df84fa53acc5108af94a8f030e6091cc629866
moobarrac <wahabajibola01@gmail.com>
2024-01-31 11:50:47 +0000

    moved message style env into the route handler

--- myApp.js
+++ myApp.js
@@ -3,13 +3,13 @@ let app = express();
 
 const publicPath = __dirname + "/public";
 const filePath = __dirname + "/views/index.html";
-const myMessageSecret = process.env["MESSAGE_STYLE"];
-const message = myMessageSecret === "uppercase" ? "HELLO JSON" : "Hello json";
 app.use("/public", express.static(publicPath));
 app.get("/", function (req, res) {
   res.sendFile(filePath);
 });
 app.get("/json", (req, res) => {
+  const myMessageSecret = process.env["MESSAGE_STYLE"];
+  const message = myMessageSecret === "uppercase" ? "HELLO JSON" : "Hello json";
   res.json({ message });
 });
 
