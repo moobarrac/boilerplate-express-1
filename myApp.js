93378ba0739b1a52f4be63b5998a6cb93c1da5e5
moobarrac <wahabajibola01@gmail.com>
2024-01-31 10:53:48 +0000

    add env to project

--- myApp.js
+++ myApp.js
@@ -3,14 +3,14 @@ let app = express();
 
 const publicPath = __dirname + "/public";
 const filePath = __dirname + "/views/index.html";
+const myMessageSecret = process.env["MESSAGE_STYLE"];
+const message = myMessageSecret === "uppercase" ? "HELLO JSON" : "Hello json";
 app.use("/public", express.static(publicPath));
 app.get("/", function (req, res) {
   res.sendFile(filePath);
 });
 app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json",
-  });
+  res.json({ message });
 });
 
 module.exports = app;
