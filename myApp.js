5257ce58df9da09981b4c20facf3cd8ec9538902
moobarrac <wahabajibola01@gmail.com>
2024-02-01 14:33:30 +0000

    get route query

--- myApp.js
+++ myApp.js
@@ -36,4 +36,9 @@ app.get("/:word/echo", (req, res) => {
   res.json({ echo: word });
 });
 
+app.route("/name").get((req, res) => {
+  const { first, last } = req.query;
+  res.json({ name: `${first} ${last}` });
+});
+
 module.exports = app;
