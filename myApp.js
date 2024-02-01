e5232d777272da328afbd87a1a2b814f99f3b50c
moobarrac <wahabajibola01@gmail.com>
2024-02-01 14:05:40 +0000

    chain a middleware and handler to route /now

--- myApp.js
+++ myApp.js
@@ -3,18 +3,32 @@ let app = express();
 
 const publicPath = __dirname + "/public";
 const filePath = __dirname + "/views/index.html";
+
 app.use(function (req, res, next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 });
 app.use("/public", express.static(publicPath));
+
 app.get("/", function (req, res) {
   res.sendFile(filePath);
 });
+
 app.get("/json", (req, res) => {
   const myMessageSecret = process.env["MESSAGE_STYLE"];
   const message = myMessageSecret === "uppercase" ? "HELLO JSON" : "Hello json";
   res.json({ message });
 });
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  },
+);
 
 module.exports = app;
